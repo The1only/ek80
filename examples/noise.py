8c0db4547542d7654aae846ab530e1a3aad9fadd
The1only <teni@KPC22002259.kongsberg.master.int>
2020-01-16 21:27:25 +0100

    Added more examples

---
+++ examples/noise.py
@@ -0,0 +1,45 @@
+# ----------------------------------------------------------------------------
+#    Method       EK80 generic v1.0
+#    Description  Subscribes to EK80 data depending on the config.xml file and report data.
+#                 Comunicate with the EK80/EK60/EK15
+#    By:          Kongsberg Maritime AS, Terje Nilsen 2020
+#-----------------------------------------------------------------------------
+import sys
+import t9ek80
+import matplotlib.pyplot as plt
+import numpy as np
+
+class ek80(t9ek80.t9ek80):
+    def __init__(self, argv):
+        super(ek80, self).__init__(argv)
+
+#----------------------------------------------------------------------------
+#   Method       report
+#   Description  User defined REPORT function, this is to be adapter to individual needs.
+#                It receives a list for parameters and meta data to process...
+#-----------------------------------------------------------------------------
+    def report(self, Payload, Decode, timenow, mtype, desimate):
+        # If biomass mode...
+        if mtype == "NoiseSpectrum":
+            plt.clf()
+            
+#            print(len(Payload))
+            t = np.arange(0.0, len(Payload)-1, 1)
+            
+            #for i in range(1,len(Payload)-1):
+            #    print("Time:    {:f}   Noise:   {:f} ".format(t[i],Payload[i]))
+                
+
+            plt.plot(t,Payload[1:])
+            plt.rcParams['axes.unicode_minus'] = True
+            plt.title('About as simple as it gets, folks')
+            plt.pause(0.05)
+
+            #plt.show()
+            
+#----------------------------------------------------------------------------
+# The main code....
+run = ek80(sys.argv)
+run.main()
+
+
