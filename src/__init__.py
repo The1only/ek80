0bc7424d60030932962eab731eab83b5f013d770
The1only <teni@KPC22002259.kongsberg.master.int>
2020-01-27 18:08:09 +0100

    Working on a pip release

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,6 @@
+from t9ek80 import __version__ as _version
+__version__ = _version
 
+def help():
+    return "This is EK80/SN90 Handler. Use it wisely."
 
--- src/setup.py
+++ src/setup.py
@@ -1,11 +1,17 @@
-import setuptools
+from setuptools import setup, find_packages
+from importlib import import_module
+
+PKG_NAME = "t9ek80"
+
+module = import_module("{}".format(PKG_NAME))
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-     name='t9ek80',  
-     version='0.3',
+setup(
+     name=PKG_NAME,
+     version=module.__version__,
      scripts=['__init__.py','t9ek80.py'] ,
      author="Terje Nilsen",
      author_email="terje.nilsen@km.kongsberg.com",
@@ -13,7 +19,7 @@ setuptools.setup(
      long_description=long_description,
    long_description_content_type="text/markdown",
      url="https://github.com/The1only/ek80",
-     packages=setuptools.find_packages(),
+     packages=find_packages(),
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
