d5feddaf2a1e2afac2701bd73a263f930ea24cc4
Terje Nilsen <teni@9tek.no>
2020-08-04 18:04:25 +0200

    Fixes Noise example...

--- examples/noise.py
+++ examples/noise.py
@@ -8,10 +8,18 @@ import sys
 import t9ek80
 import matplotlib.pyplot as plt
 import numpy as np
+import multiprocessing as mp
 
 class ek80(t9ek80.t9ek80):
     def __init__(self, argv):
         super(ek80, self).__init__(argv)
+        
+        self.plot_pipe, plotter_pipe = mp.Pipe()
+        self.plotter = ProcessPlotter()
+        self.plot_process = mp.Process(
+            target=self.plotter, args=(plotter_pipe,), daemon=True)
+        self.plot_process.start()
+        
 #-----------------------------------------------------------------------------
 # Override the function getDebug, defaut if no overide is False. 
     def getDebug(self):
@@ -24,13 +32,42 @@ class ek80(t9ek80.t9ek80):
     def report(self, Payload, Decode, timenow, mtype, desimate, transponder, unit, product):
         # If NoiseSpectrum mode...
         if mtype == "NoiseSpectrum":
-            plt.clf()
+            send = self.plot_pipe.send
             t = np.arange(0.0, len(Payload)-1, 1)
-            plt.plot(t,Payload[1:])
-            plt.rcParams['axes.unicode_minus'] = True
-            plt.title('About as simple as it gets, folks')
-            plt.pause(0.05)
-            
+            send([t,Payload[1:]])
+
+#----------------------------------------------------------------------------
+# As we now are in a different process we must do this trick to be alowed to plot...
+class ProcessPlotter(object):
+    def __init__(self):
+        self.x = []
+        self.y = []
+
+    def terminate(self):
+        plt.close('all')
+
+    def call_back(self):
+        while self.pipe.poll():
+            command = self.pipe.recv()
+            if command is None:
+                self.terminate()
+                return False
+            else:
+                self.x = command[0]
+                self.y = command[1]
+                self.ax.clear()
+                self.ax.plot(self.x, self.y) #, 'ro')
+        self.fig.canvas.draw()
+        return True
+
+    def __call__(self, pipe):
+        self.pipe = pipe
+        self.fig, self.ax = plt.subplots()
+        timer = self.fig.canvas.new_timer(interval=100)
+        timer.add_callback(self.call_back)
+        timer.start()
+        plt.show()
+
 #----------------------------------------------------------------------------
 # The main code....
 run = ek80(sys.argv)
